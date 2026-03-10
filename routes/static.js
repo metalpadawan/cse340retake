c90854f8656f244c4e29412ecfd0622404a00fc8
metalpadawan <emmanuelndem9@gmail.com>
2026-03-10 17:52:33 +0100

    update to frontend

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+// Serve static assets from /public (css, js, images)
+router.use(express.static("public"))
 
-module.exports = router;
-
-
-
+module.exports = router
