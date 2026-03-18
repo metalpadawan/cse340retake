b91d13cd70a282891d115e488ab1d59a1fb3a654
metalpadawan <emmanuelndem9@gmail.com>
2026-03-18 17:27:21 +0100

    Inventory Delivery By Classification Activity

--- server.js
+++ server.js
@@ -5,12 +5,14 @@
 /* ***********************
  * Require Statements
  *************************/
+const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const utilities = require("./utilities/")
 
 /* ***********************
  * Routes
@@ -18,14 +20,23 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") //not at views root
+
+app.use(async (req, res, next) => {
+  try {
+    res.locals.nav = await utilities.getNav()
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
