ee1e1c7f614f67b8c4e355575436c14cb96bc589
metalpadawan <emmanuelndem9@gmail.com>
2026-03-23 16:57:15 +0100

    sessions and flash messages

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,6 +4,7 @@ const baseController = {}
 // Build the home page and send the shared navigation to the view.
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  req.flash("notice", "This is a flash message.")
   res.render("index", {title: "Home", nav})
 }
 
